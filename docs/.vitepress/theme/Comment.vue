862cbc9d8b1cf8fc054fb0c8ac13fdbf38c2d99a
TuNan <xiahaike@icloud.com>
2023-12-18 18:06:55 +0800

    新增评论giscus

---
+++ docs/.vitepress/theme/Comment.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="comment">
+    <component
+        v-if="isDark"
+        :is="'script'"
+        src="https://giscus.app/client.js"
+        data-repo="Tunan81/blog"
+        data-repo-id="R_kgDOK2xPmw"
+        data-category="General"
+        data-category-id="DIC_kwDOK2xPm84Cb1TH"
+        data-mapping="pathname"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="dark"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        data-loading="eager"
+        async
+    >
+    </component>
+    <component
+        v-else
+        :is="'script'"
+        src="https://giscus.app/client.js"
+        data-repo="Tunan81/blog"
+        data-repo-id="R_kgDOK2xPmw"
+        data-category="General"
+        data-category-id="DIC_kwDOK2xPm84Cb1TH"
+        data-mapping="pathname"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="light"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        data-loading="eager"
+        async
+    >
+    </component>
+  </div>
+</template>
+<script setup lang="ts">
+import { useData } from 'vitepress';
+// 获取当前配色方案
+const { isDark } = useData();
+</script>
+<style lang="scss" scoped>
+.comment {
+  margin-top: 16px;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,20 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import {h} from 'vue'
+import type {Theme} from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+// @ts-ignore
+import Comment from "./Comment.vue";
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+    extends: DefaultTheme,
+    Layout: () => {
+        return h(DefaultTheme.Layout, null, {
+            // https://vitepress.dev/guide/extending-default-theme#layout-slots
+            'doc-after': () => h(Comment), // 导入评论组件
+        })
+    },
+    enhanceApp({app, router, siteData}) {
+        // ...
+    }
 } satisfies Theme
